a9617c5b21d9a4320344b2362f46a8e4dcff6372
Swyter <swyterzone@gmail.com>
2022-05-04 23:25:02 +0200

    Add the Sphinx and the Cursed Mummy hashcodes, it also comes with some common EngineX stuff.

---
+++ enginex/albert/hashcode_animbone.h
@@ -0,0 +1,133 @@
+/* Schema: 0001 */
+/* HT_AnimBone */
+#define HT_AnimBone_HASHCODE_BASE		0x0e000000
+
+#define HT_AnimBone_Pelvis				0x0e000001
+#define HT_AnimBone_Spine				0x0e000002
+#define HT_AnimBone_Spine1				0x0e000003
+#define HT_AnimBone_Spine2				0x0e000004
+#define HT_AnimBone_Spine3				0x0e000005
+#define HT_AnimBone_Neck				0x0e000006
+#define HT_AnimBone_Neck1				0x0e000007
+#define HT_AnimBone_Neck2				0x0e000008
+#define HT_AnimBone_Head				0x0e000009
+#define HT_AnimBone_Ponytail1			0x0e00000a
+#define HT_AnimBone_Ponytail11			0x0e00000b
+#define HT_AnimBone_Ponytail2			0x0e00000c
+#define HT_AnimBone_Ponytail21			0x0e00000d
+#define HT_AnimBone_L_Clavicle			0x0e00000e
+#define HT_AnimBone_L_UpperArm			0x0e00000f
+#define HT_AnimBone_L_Forearm			0x0e000010
+#define HT_AnimBone_L_Hand				0x0e000011
+#define HT_AnimBone_R_Clavicle			0x0e000012
+#define HT_AnimBone_R_UpperArm			0x0e000013
+#define HT_AnimBone_R_Forearm			0x0e000014
+#define HT_AnimBone_R_Hand				0x0e000015
+#define HT_AnimBone_L_Thigh				0x0e000016
+#define HT_AnimBone_L_Calf				0x0e000017
+#define HT_AnimBone_L_Foot				0x0e000018
+#define HT_AnimBone_R_Thigh				0x0e000019
+#define HT_AnimBone_R_Calf				0x0e00001a
+#define HT_AnimBone_R_Foot				0x0e00001b
+#define HT_AnimBone_Tail				0x0e00001c
+#define HT_AnimBone_Tail1				0x0e00001d
+#define HT_AnimBone_Tail2				0x0e00001e
+#define HT_AnimBone_Tail3				0x0e000020
+#define HT_AnimBone_Tail4				0x0e000021
+
+#define HT_AnimBone_L_Eye				0x0e000022
+#define HT_AnimBone_R_Eye				0x0e000023
+#define HT_AnimBone_L_Eyebrow			0x0e000024
+#define HT_AnimBone_R_Eyebrow		   	0x0e000025
+#define HT_AnimBone_L_Ear				0x0e000026
+#define HT_AnimBone_R_Ear				0x0e000027
+#define HT_AnimBone_L_HorseLink			0x0e000028
+#define HT_AnimBone_R_HorseLink			0x0e000029
+
+#define HT_AnimBone_L_Finger0			0x0e000030
+#define HT_AnimBone_L_Finger01			0x0e000031
+#define HT_AnimBone_L_Finger02			0x0e000032
+#define HT_AnimBone_L_Finger1			0x0e000033
+#define HT_AnimBone_L_Finger11			0x0e000034
+#define HT_AnimBone_L_Finger12			0x0e000035
+#define HT_AnimBone_L_Finger2			0x0e000036
+#define HT_AnimBone_L_Finger21			0x0e000037
+#define HT_AnimBone_L_Finger22			0x0e000038
+#define HT_AnimBone_L_Finger3			0x0e000039
+#define HT_AnimBone_L_Finger31			0x0e00003a
+#define HT_AnimBone_L_Finger32			0x0e00003b
+#define HT_AnimBone_L_Finger4			0x0e00003c
+#define HT_AnimBone_L_Finger41			0x0e00003d
+#define HT_AnimBone_L_Finger42			0x0e00003e
+#define HT_AnimBone_R_Finger0			0x0e00003f
+#define HT_AnimBone_R_Finger01			0x0e000040
+#define HT_AnimBone_R_Finger02			0x0e000041
+#define HT_AnimBone_R_Finger1			0x0e000042
+#define HT_AnimBone_R_Finger11			0x0e000043
+#define HT_AnimBone_R_Finger12			0x0e000044
+#define HT_AnimBone_R_Finger2			0x0e000045
+#define HT_AnimBone_R_Finger21			0x0e000046
+#define HT_AnimBone_R_Finger22			0x0e000047
+#define HT_AnimBone_R_Finger3			0x0e000048
+#define HT_AnimBone_R_Finger31			0x0e000049
+#define HT_AnimBone_R_Finger32			0x0e00004a
+#define HT_AnimBone_R_Finger4			0x0e00004b
+#define HT_AnimBone_R_Finger41			0x0e00004c
+#define HT_AnimBone_R_Finger42			0x0e00004d
+
+#define HT_AnimBone_L_Toe0				0x0e000050
+#define HT_AnimBone_L_Toe01				0x0e000051
+#define HT_AnimBone_L_Toe02				0x0e000052
+#define HT_AnimBone_L_Toe1				0x0e000053
+#define HT_AnimBone_L_Toe11				0x0e000054
+#define HT_AnimBone_L_Toe12				0x0e000055
+#define HT_AnimBone_L_Toe2				0x0e000056
+#define HT_AnimBone_L_Toe21				0x0e000057
+#define HT_AnimBone_L_Toe22				0x0e000058
+#define HT_AnimBone_L_Toe3				0x0e000059
+#define HT_AnimBone_L_Toe31				0x0e00005a
+#define HT_AnimBone_L_Toe32				0x0e00005b
+#define HT_AnimBone_L_Toe4				0x0e00005c
+#define HT_AnimBone_L_Toe41				0x0e00005d
+#define HT_AnimBone_L_Toe42				0x0e00005e
+#define HT_AnimBone_R_Toe0				0x0e00005f
+#define HT_AnimBone_R_Toe01				0x0e000060
+#define HT_AnimBone_R_Toe02				0x0e000061
+#define HT_AnimBone_R_Toe1				0x0e000062
+#define HT_AnimBone_R_Toe11				0x0e000063
+#define HT_AnimBone_R_Toe12				0x0e000064
+#define HT_AnimBone_R_Toe2				0x0e000065
+#define HT_AnimBone_R_Toe21				0x0e000066
+#define HT_AnimBone_R_Toe22				0x0e000067
+#define HT_AnimBone_R_Toe3				0x0e000068
+#define HT_AnimBone_R_Toe31				0x0e000069
+#define HT_AnimBone_R_Toe32				0x0e00006a
+#define HT_AnimBone_R_Toe4				0x0e00006b
+#define HT_AnimBone_R_Toe41				0x0e00006c
+#define HT_AnimBone_R_Toe42				0x0e00006d
+
+#define HT_AnimBone_Object01			0x0e000070
+#define HT_AnimBone_Object02			0x0e000071
+#define HT_AnimBone_Object03			0x0e000072
+#define HT_AnimBone_Object04			0x0e000073
+#define HT_AnimBone_Object05			0x0e000074
+#define HT_AnimBone_Object06			0x0e000075
+#define HT_AnimBone_Object07			0x0e000076
+#define HT_AnimBone_Object08			0x0e000077
+
+/* 0e000056 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
