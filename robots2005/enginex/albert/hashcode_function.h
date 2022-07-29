ce87e1de314da4676647c911df23f0c775e7002a
jmarti856 <jmarti856@gmail.com>
2022-07-29 13:22:44 +0200

    Add the Robots 2005 hashcodes

---
+++ robots2005/enginex/albert/hashcode_function.h
@@ -0,0 +1,24 @@
+/* Schema: 0001 */
+/* HT_Function */
+#define HT_Function_HASHCODE_BASE		0x0c000000
+#define HT_Function_Test		 0x0c000000
+#define HT_Function_Move		 0x0c000001
+#define HT_Function_Blend		 0x0c000002
+#define HT_Function_IdleBlend		 0x0c000003
+#define HT_Function_Change		 0x0c000004
+#define HT_Function_SnapshotAll 	 0x0c000005
+#define HT_Function_HandlerFunction 	 0x0c000006
+/* 0c000007 */
+
+
+
+
+
+
+
+
+
+
+
+
+
